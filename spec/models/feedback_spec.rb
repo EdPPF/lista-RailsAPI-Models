dc9d6c903173636bca578c65567eeb4afc38b221
EduardoPPF <ppfeduardo@gmail.com>
2023-06-07 14:12:04 -0300

    Teste factory de Feedback: Falha (criado sem um post)

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Feedback, type: :model do
   context "Testing factory" do
-    it "should be valid" do
+    it "should create feedback" do
       expect(build(:feedback)). to be_valid
     end
   end
