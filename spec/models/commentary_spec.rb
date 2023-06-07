c4ab6c1cbb66bab46cf6f51563cb96fda4ef0d67
EduardoPPF <ppfeduardo@gmail.com>
2023-06-07 14:10:33 -0300

    Teste factory de Commentary: Sucesso

--- spec/models/commentary_spec.rb
+++ spec/models/commentary_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Commentary, type: :model do
   context "Testing factory" do
-    it "should be valid" do
-      expect(build(:commentary)).to be_valid
+    it "should create commentary" do
+      create(:post, id:1)
+      expect(build(:commentary, post_id:1)).to be_valid
     end
   end
 
