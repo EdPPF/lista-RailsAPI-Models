5d4f34d06988013eb0e61a4c80eece5a620fac28
EduardoPPF <ppfeduardo@gmail.com>
2023-06-07 11:10:42 -0300

    Teste de factory para Post: Sucesso

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+
+  context "Testing factory" do
+    it "should be valid" do
+      expect(build(:post)).to be_valid
+    end
+  end
+
   context "Validating title" do
     it "should be invalid if title is nil" do
       expect(build(:post, title:nil)).to be_invalid
@@ -8,10 +15,9 @@ RSpec.describe Post, type: :model do
   end
 
   context "Validating content" do
-    it "should be invalid if title is nil" do
+    it "should be invalid if content is nil" do
       expect(build(:post, content:nil)).to be_invalid
     end
   end
 
-  # user_admin_id not nil
 end
