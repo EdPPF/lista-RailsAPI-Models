835e2697d5921c4438b166cc4a4732e4599dc225
EduardoPPF <ppfeduardo@gmail.com>
2023-06-07 11:00:02 -0300

    Teste de content para Post: Falha

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Post, type: :model do
       expect(build(:post, title:nil)).to be_invalid
     end
   end
+
+  context "Validating content" do
+    it "should be invalid if title is nil" do
+      expect(build(:post, content:nil)).to be_invalid
+    end
+  end
+
+  # user_admin_id not nil
 end
