414e6071ffbcc360ac7faa6e68579cac0763b844
EduardoPPF <ppfeduardo@gmail.com>
2023-06-07 15:23:27 -0300

    Teste factory de PostCategory: Sucesso

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe PostCategory, type: :model do
   context "Testing factory" do
     it "should create Post Category" do
       create(:post, id:1)
-      expect(build(:post_category, post_id:1)).to be_valid
+      create(:category, id:1)
+      expect(build(:post_category, post_id:1, category_id:1)).to be_valid
     end
   end
 
