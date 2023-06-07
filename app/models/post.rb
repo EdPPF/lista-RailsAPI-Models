a16157c3f32f9b80e5072aa5b7b7a8a6b86f0367
EduardoPPF <ppfeduardo@gmail.com>
2023-06-07 15:25:22 -0300

    Finalizando: Posy e Category "has_many" Post_Category

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     has_many :commentaries, dependent: :destroy
     has_many :feedbacks, dependent: :destroy
+    has_many :post_category, dependent: :destroy
 
     validates :title, presence: :true
     validates :content, presence: :true
