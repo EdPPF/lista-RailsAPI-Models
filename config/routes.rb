87233456828b7c84e82b02dd506bd4ef8b1037dc
EduardoPPF <ppfeduardo@gmail.com>
2023-06-14 16:27:03 -0300

    Post Controlle -  Teste POST/create: falha

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  # <método HTTPS> '<método rails (CRUD)>'
   namespace 'api' do
     namespace 'v1' do # create, index, show, update e delete;
       scope 'post' do
-        post 'create', to:'posts#create'
-        get 'index', to:'posts#index'
-        get 'show', to:'posts:show'
-        patch 'update/:id', to:'posts#update'
-        delete 'delete/:id', to:'posts#delete'
+        post 'create', to:'post#create'
+        get 'index', to:'post#index'
+        get 'show/:id', to:'post#show'
+        patch 'update/:id', to:'post#update'
+        delete 'delete/:id', to:'post#delete'
       end
     end
   end
--- spec/requests/api/v1/post_spec.rb
+++ spec/requests/api/v1/post_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Posts", type: :request do
+RSpec.describe "Api::V1::Post", type: :request do
   describe "POST / create" do
     let(:post_params) do
       attributes_for(:post)
     end
     context "when params are ok" do
       it "return http status created" do
-        post "/api/v1/posts/create", params:{post: post_params}
+        post "/api/v1/post/create", params:{post: post_params}
         expect(response).to have_http_status(:created)
       end
     end
