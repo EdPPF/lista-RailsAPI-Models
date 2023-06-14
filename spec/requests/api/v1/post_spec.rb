5a4ed9aca69a35487c1418aa56008909e122a06e
EduardoPPF <ppfeduardo@gmail.com>
2023-06-14 16:45:44 -0300

    Post Controller -  Mais Testes POST/create: sucessos

--- spec/requests/api/v1/post_spec.rb
+++ spec/requests/api/v1/post_spec.rb
@@ -11,5 +11,14 @@ RSpec.describe "Api::V1::Post", type: :request do
         expect(response).to have_http_status(:created)
       end
     end
+
+    context "when params are nil" do
+      post_params = nil
+      it "return http status bad_request" do
+        post "/api/v1/post/create", params:{post: post_params}
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+
   end
 end
