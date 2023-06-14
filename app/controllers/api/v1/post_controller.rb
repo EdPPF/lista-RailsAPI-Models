05ea647698a1ac685b88f555049aee3f8498ee74
EduardoPPF <ppfeduardo@gmail.com>
2023-06-14 16:38:21 -0300

    Post Controller -  Teste POST/create: sucesso

--- app/controllers/api/v1/post_controller.rb
+++ app/controllers/api/v1/post_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::PostController < ApplicationController
+  def create
+    post = Post.new(post_params)
+    post.save!
+    render json: post, status: :created
+  rescue StandardError => e
+    render json: e, status: :bad_request
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content, :useradmin_id)
+    end
 end
