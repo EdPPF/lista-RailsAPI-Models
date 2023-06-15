5d77f5ccf44f1e048abf180bf0a6f70cdb176a6a
EduardoPPF <ppfeduardo@gmail.com>
2023-06-15 17:35:27 -0300

    Post Controller -  Teste GET/show: sucesso

--- app/controllers/api/v1/post_controller.rb
+++ app/controllers/api/v1/post_controller.rb
@@ -7,6 +7,13 @@ class Api::V1::PostController < ApplicationController
     render json: e, status: :bad_request
   end
 
+  def show
+    pos = Post.find(params[:id])
+    render json: pos, status: :ok
+  rescue StandardError => e
+    render json: e, status: :not_found
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :content, :useradmin_id)
