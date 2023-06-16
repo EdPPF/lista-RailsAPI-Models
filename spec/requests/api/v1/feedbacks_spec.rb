45a4d1a0a930750198412896d437dc286a1067d3
EduardoPPF <ppfeduardo@gmail.com>
2023-06-16 15:47:25 -0300

    Feedbacks Controller -  Testes GET/index: falhas

--- spec/requests/api/v1/feedbacks_spec.rb
+++ spec/requests/api/v1/feedbacks_spec.rb
@@ -14,4 +14,23 @@ RSpec.describe "Api::V1::Feedbacks", type: :request do
       end
     end
   end
+
+  describe "GET / index" do
+    before do
+      create(:post, id: 2, title:"POSTE", content:"contente")
+      create(:feedback, like: true, user_id: 42, post_id: 2)
+    end
+
+    context "when feedback is created" do
+      it "returns http status ok" do
+        get "/api/v1/feedbacks/index"
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "returns a JSON" do
+        get "/api/v1/feedbacks/index"
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+  end
 end
