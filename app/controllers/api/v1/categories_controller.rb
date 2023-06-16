3c70e7bf5569749590686fc2febd0841c67ee824
EduardoPPF <ppfeduardo@gmail.com>
2023-06-16 14:03:49 -0300

    Categories Controller -  Testes POST/create: sucessos

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::CategoriesController < ApplicationController
+  def create
+    categor = Category.new(category_params)
+    categor.save!
+    render json: categor, status: :created
+  rescue StandardError => e
+    render json: e, status: :bad_request
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name, :description)
+    end
 end
