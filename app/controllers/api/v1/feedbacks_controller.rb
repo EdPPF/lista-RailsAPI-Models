d7307e2e4cc768ce8c6f46c7962ac297f187a4fe
EduardoPPF <ppfeduardo@gmail.com>
2023-06-16 15:42:16 -0300

    Feedbacks Controller -  Teste POST/create: sucesso

--- app/controllers/api/v1/feedbacks_controller.rb
+++ app/controllers/api/v1/feedbacks_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::FeedbacksController < ApplicationController
-  # a fazer
-  #
+  def create
+    feed = Feedback.new(feedback_params)
+    feed.save!
+    render json: feed, status: :created
+  rescue StandardError => e
+    render json: e, status: :bad_request
+  end
+
+  private
+    def feedback_params
+      params.require(:feedback).permit(:like, :user_id, :post_id)
+    end
 end
--- spec/requests/api/v1/feedbacks_spec.rb
+++ spec/requests/api/v1/feedbacks_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Feedbacks", type: :request do
   describe "POST / create" do
-    let(:feedback_params) do
-      attributes_for(:feedback)
+    before do
+      create(:post, id: 1, title:"POSTE", content:"contente")
     end
+    feedback_params = {like: true, user_id: 42, post_id: 1}
 
     context "when params are ok" do
       it "return http status created" do
