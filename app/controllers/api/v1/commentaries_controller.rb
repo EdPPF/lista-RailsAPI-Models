f4de78ab18d08540c1a404a9e5f17e35caa5fdc7
EduardoPPF <ppfeduardo@gmail.com>
2023-06-16 16:59:10 -0300

    Commentaries Controller - Testes POST/create: sucessos

--- app/controllers/api/v1/commentaries_controller.rb
+++ app/controllers/api/v1/commentaries_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::CommentariesController < ApplicationController
+  def create
+    commen = Commentary.new(commentary_params)
+    commen.save!
+    render json: commen, status: :created
+  rescue StandardError => e
+    render json: e, status: :bad_request
+  end
 
   private
     def commentary_params
-      params.require(:commentary).permit(:name, :description)
+      params.require(:commentary).permit(:content, :user_id, :post_id)
     end
 end
--- spec/requests/api/v1/commentaries_spec.rb
+++ spec/requests/api/v1/commentaries_spec.rb
@@ -4,20 +4,24 @@ RSpec.describe "Api::V1::Commentaries", type: :request do
   describe "POST / create" do
     before do
       create(:post, id: 1, title:"Titulo1", content:"Conteudo1")
-      create(:commentary, content:"McDonalds", user_id:20, post_id:1)
+      #create(:commentary, content:"McDonalds", user_id:20, post_id:1)
+    end
+    let(:commentary_params) do
+      attributes_for(:commentary)
     end
 
     context "when params are ok" do
+      commentary_params = {post_id: 1, user_id:20, content:"AA"}
       it "return http status created" do
-        post "/api/v1/commentaries/create"
+        post "/api/v1/commentaries/create", params:{commentary: commentary_params}
         expect(response).to have_http_status(:created)
       end
     end
 
     context "when params are nil" do
-      post_params = nil
+      commentary_params = nil
       it "return http status bad_request" do
-        post "/api/v1/commentaries/create", params:{post: post_params}
+        post "/api/v1/commentaries/create", params:{commentary: commentary_params}
         expect(response).to have_http_status(:bad_request)
       end
     end
